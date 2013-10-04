6b0a9738fcbebfeaac3b3bfbdbf0ec904567a566
Adrian Mowat <adrian.mowat@arnoldclark.co.uk>
2013-10-04 17:28:56 +0100

    Created functions to allow us to render a card on a date

--- src/kanban_metrics/web/views/board.clj
+++ src/kanban_metrics/web/views/board.clj
@@ -17,6 +17,28 @@
 ;      (filter #(t/before? (DateTime. today) (DateTime. (:card/done %))))
 ;      count)
 
+(def date-columns [:card/requested
+                   :card/in-progress
+                   :card/uat
+                   :card/done
+                   :card/backlog
+                   :card/qa])
+
+(def meta-columns [:card/sequence :card/description :card/work-item-type])
+
+(def all-cards (cards (card-ids)))
+(def card (first all-cards))
+
+(defn dates-for [card]
+  (reduce #(conj %1 (%2 card)) #{} date-columns))
+
+(defn get-columns-for-date [card date]
+  (filter #(= (%1 card) date) date-columns))
+
+(defn get-card [card date]
+  (-> (select-keys card meta-columns)
+      (assoc :card/current (last (get-columns-for-date card date)))))
+
 (defn show [columns cards]
   (html [:body
           [:h1 "Kanban Board"]]))
